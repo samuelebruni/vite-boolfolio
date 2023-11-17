5f12d7100a28cc27cadc3d072488f8b50e6b8e99
samuelebruni <samuele_bruni@hotmail.com>
2023-11-17 17:39:35 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script>
+import axios from 'axios';
+import ProjectCard from './components/ProjectCard.vue'
+
+export default {
+  name: 'App',
+  data() {
+
+    return {
+      baseUrl: 'http://127.0.0.1:8000/',
+      projectApi: 'api/project'
+    }
+
+  },
+
+  mounted() {
+    axios.get(this.baseUrl + this.projectApi)
+      .then(response => {
+        console.log(response);
+        this.projects = response.data.result.data
+      }).catch(err => {
+        console.error(err);
+      })
+  }
+
+}
+</script>
+
+<template></template>
+
