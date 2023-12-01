7fc5588eb5bd707998d930219b0f730c409bd3ef
samuelebruni <samuele_bruni@hotmail.com>
2023-12-01 11:01:36 +0100

    add form function

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -11,11 +11,15 @@ export default {
             email: '',
             phone: '',
             message: '',
-
+            errors: [],
+            success: null
         }
     },
     methods: {
         sendForm() {
+            this.loading = true;
+            this.errors = [];
+            this.success = null
             const payload = {
                 name: this.name,
                 email: this.email,
@@ -25,7 +29,22 @@ export default {
             console.log(payload);
             axios.post(this.baseUrl + this.contactsApi, payload)
                 .then(response => {
-                    console.log(response);
+                    const success = response.data.success
+                    if (!success) {
+                        console.log(response);
+                        console.log(response.data.errors);
+                        this.errors = response.data.errors
+                    } else {
+                        console.log(response);
+                        console.log(response.data.message);
+                        this.name = ''
+                        this.email = ''
+                        this.message = ''
+                        this.phone = ''
+
+                        this.success = response.data.message
+                    }
+                    this.loading = false
                 })
                 .catch(error => {
                     console.error(error.message);
@@ -45,35 +64,106 @@ export default {
         </div>
 
 
-        <div class="container my-5">
+        <div class="container">
             <form action="" v-on:submit.prevent="sendForm()">
-                <div class="mb-3">
-                    <label for="name" class="form-label text-uppercase">Name</label>
-                    <input type="text" name="name" id="name" class="form-control" placeholder="Elon Musk"
-                        aria-describedby="nameHelper" v-model="name">
-                    <small id="nameHelper" class="text-muted">Type your name</small>
+                <div v-if="!loading">
+                    <div class="mb-3">
+                        <label for="name" class="form-label text-uppercase">Name</label>
+                        <input type="text" name="name" id="name" class="form-control" placeholder="Elon Musk"
+                            aria-describedby="nameHelper" v-model="name" :class="{ 'is-invalid': errors.name }">
+                        <small id="nameHelper" class="text-muted">Type your name</small>
+
+                        <div class="alert alert-danger" role="alert" v-if="errors.name">
+                            <strong>Erorrs!</strong>
+
+                            <ul>
+                                <li v-for="message in errors.name">{{ message }}</li>
+                            </ul>
+
+                        </div>
+
+                    </div>
+
+                    <div class="mb-3">
+                        <label for="phone" class="form-label  text-uppercase">phone</label>
+                        <input type="tel" name="phone" id="phone" class="form-control" placeholder="123456"
+                            aria-describedby="phoneHelper" v-model="phone" :class="{ 'is-invalid': errors.phone }">
+                        <small id="phoneHelper" class="text-muted">Type your phone</small>
+
+                        <div class="alert alert-danger" role="alert" v-if="errors.phone">
+                            <strong>Erorrs!</strong>
+
+                            <ul>
+                                <li v-for="message in errors.phone">{{ message }}</li>
+                            </ul>
+
+                        </div>
+                    </div>
+                    <div class="mb-3">
+                        <label for="email" class="form-label text-uppercase">email</label>
+                        <input type="text" name="email" id="email" class="form-control" placeholder="elon@musk.com"
+                            aria-describedby="emailHelper" v-model="email" :class="{ 'is-invalid': errors.email }">
+                        <small id="emailHelper" class="text-muted">Type your email</small>
+
+                        <div class="alert alert-danger" role="alert" v-if="errors.email">
+                            <strong>Erorrs!</strong>
+
+                            <ul>
+                                <li v-for="message in errors.email">{{ message }}</li>
+                            </ul>
+
+                        </div>
+                    </div>
+
+                    <div class="mb-3">
+                        <label for="message" class="form-label">Message</label>
+                        <textarea class="form-control" name="message" id="message" rows="3"
+                            placeholder="Your message here..." v-model="message"
+                            :class="{ 'is-invalid': errors.message }"></textarea>
+
+                        <div class="alert alert-danger" role="alert" v-if="errors.message">
+                            <strong>Erors!</strong>
+
+                            <ul>
+                                <li v-for="message in errors.message">{{ message }}</li>
+                            </ul>
+
+                        </div>
+                    </div>
+
+                    <button type="submit" class="btn mb-3" :class="{ ' btn-primary w-100': !loading }" :disabled="loading">
+                        <span v-if="loading">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                class="bi bi-send" viewBox="0 0 16 16">
+                                <path
+                                    d="M15.854.146a.5.5 0 0 1 .11.54l-5.819 14.547a.75.75 0 0 1-1.329.124l-3.178-4.995L.643 7.184a.75.75 0 0 1 .124-1.33L15.314.037a.5.5 0 0 1 .54.11ZM6.636 10.07l2.761 4.338L14.13 2.576zm6.787-8.201L1.591 6.602l4.339 2.76 7.494-7.493Z" />
+                            </svg>
+                        </span>
+                        <span v-else>
+                            Send
+                        </span>
+
+                    </button>
                 </div>
-                <div class="mb-3">
-                    <label for="phone" class="form-label  text-uppercase">phone</label>
-                    <input type="tel" name="phone" id="phone" class="form-control" placeholder="123456"
-                        aria-describedby="phoneHelper" v-model="phone">
-                    <small id="phoneHelper" class="text-muted">Type your phone</small>
-                </div>
-                <div class="mb-3">
-                    <label for="email" class="form-label text-uppercase">email</label>
-                    <input type="text" name="email" id="email" class="form-control" placeholder="elon@musk.com"
-                        aria-describedby="emailHelper" v-model="email">
-                    <small id="emailHelper" class="text-muted">Type your email</small>
+                <div class="loader text-center py-5" v-else>
+
+                    <i class="fa-solid fa-spinner fa-spin-pulse fa-2xl"></i>
+                    <div class="mt-3">
+                        Loading...
+                    </div>
+
+
                 </div>
 
-                <div class="mb-3">
-                    <label for="message" class="form-label">Message</label>
-                    <textarea class="form-control" name="message" id="message" rows="3" placeholder="Your message here..."
-                        v-model="message"></textarea>
+
+
+
+                <div class="alert alert-success" role="alert" v-if="success">
+                    <strong>
+                        {{ success }}
+                    </strong>
                 </div>
 
-
-                <button type="submit" class="btn btn-primary mb-3">Send</button>
 
             </form>
         </div>
